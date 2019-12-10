39e5e5fc0c896ba899cd73b17f2a91ab0f8f7182
gpf131012 <1654648712@qq.com>
2019-12-10 20:23:32 +0800

    发布文章评论-文章评论点赞

--- src/views/article/components/article-comment.vue
+++ src/views/article/components/article-comment.vue
@@ -9,7 +9,7 @@
     >
       <van-cell
         v-for="item in list"
-        :key="item"
+        :key="item.com_id.toString()"
         :title="item"
       >
         <van-image
@@ -18,17 +18,25 @@
           width="30"
           height="30"
           style="margin-right: 10px;"
-          src="https://img.yzcdn.cn/vant/cat.jpeg"
+          :src="item.aut_photo"
         />
-        <span style="color: #466b9d;" slot="title">hello</span>
+        <span style="color: #466b9d;"
+        slot="title"
+        >{{item.aut_name}}</span>
         <div slot="label">
-          <p style="color: #363636;">我出去跟别人说我的是。。。</p>
+          <p style="color: #363636;">{{item.content}}</p>
           <p>
-            <span style="margin-right: 10px;">3天前</span>
+            <span style="margin-right: 10px;">{{item.pubdate | relativeTime}}</span>
             <van-button size="mini" type="default">回复</van-button>
           </p>
         </div>
-        <van-icon slot="right-icon" name="like-o" />
+        <van-icon
+        slot="right-icon"
+        color="red"
+        :name="item.is_liking ? 'like' : 'like-o'"
+        @click="onCommentLike(item)"
+
+        />
       </van-cell>
     </van-list>
     <!-- 评论列表 -->
@@ -37,9 +45,15 @@
     <van-cell-group class="publish-wrap">
       <van-field
         clearable
+        v-model="inputComment"
         placeholder="请输入评论内容"
       >
-        <van-button slot="button" size="mini" type="info">发布</van-button>
+        <van-button
+        slot="button"
+        size="mini"
+        type="info"
+        @click="onAddComment"
+        >发布</van-button>
       </van-field>
     </van-cell-group>
     <!-- /发布评论 -->
@@ -47,6 +61,8 @@
 </template>
 
 <script>
+import { getComments, addComment, addCommentlike, deleteCommentlike } from '@/api/comment'
+
 export default {
   name: 'ArticleComment',
   props: {},
@@ -54,25 +70,55 @@ export default {
     return {
       list: [], // 评论列表
       loading: false, // 上拉加载更多的 loading
-      finished: false // 是否加载结束
+      finished: false, // 是否加载结束
+      offset: null, // 获取下一页数据评论页码
+      inputComment: ''
     }
   },
 
   methods: {
-    onLoad () {
-      // 异步更新数据
-      setTimeout(() => {
-        for (let i = 0; i < 10; i++) {
-          this.list.push(this.list.length + 1)
-        }
-        // 加载状态结束
-        this.loading = false
-
-        // 数据全部加载完成
-        if (this.list.length >= 40) {
-          this.finished = true
-        }
-      }, 500)
+    async onCommentLike (comment) {
+      if (comment.is_liking) {
+        await deleteCommentlike(comment.com_id)
+      } else {
+        // 如果没有赞，则点赞
+        await addCommentlike(comment.com_id)
+      }
+      // 更新视图状态
+      comment.is_liking = !comment.is_liking
+      this.$toast('操作成功')
+    },
+    async onAddComment () {
+      const inputComment = this.inputComment.trim()
+      // 非空校验
+      if (!inputComment.length) {
+        return
+      }
+      const res = await addComment({
+        target: this.$route.params.articleId // 评论的目标id（评论文章即为文章id，对评论进行回复则回复为评论id）
+      })
+      this.list.puth(res.data.data.new_obj)
+    },
+    async onLoad () {
+      // 1，请求获取数据
+      const res = await getComments({
+        type: 'a', // a或c，评论类型，a-对文章（article）的评论，c-对评论（comment）的回复
+        source: this.$route.params.articleId, // 源id，文章或评论id
+        offset: this.offset
+        // 值为评论id，表示从此id的数据向后取，不传表示 从第一页开始读取数据
+      })
+      // 2. 将数据添加到列表中
+      this.list.push(...res.data.data.results)
+      // 3.关闭转圈圈
+      this.loading = false
+      // 4.判断如果没有数据了，则亮finished设置为true
+      const lastId = res.data.data.last_id
+      if (lastId) {
+        // 更新获取西亚一页数据的页码（偏移量）
+        this.offset = lastId
+      } else {
+        this.finished = true
+      }
     }
   }
 }
